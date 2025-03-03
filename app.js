b59a1575824f0d7ea672c1cdb894bcf15c902f2a
victordanielmencia <victordanielmencia@gmail.com>
2025-03-02 22:10:50 -0300

    Creadas las funciones agregarAmigo y agregarALista

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    let listaAmigos = document.getElementById('listaAmigos');
+    if (nombreAmigo == '') {
+        alert('Por favor, ingrese un nombre valido');
+    } else {
+        amigos.push(nombreAmigo);
+        agregarALista();
+        document.getElementById('amigo').value = '';
+    }
+}
+
+function agregarALista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+
+}
